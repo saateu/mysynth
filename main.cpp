014a1f76d48765a038b659f4383bc78ba3609493
Shinya.Saito <saateu@gmail.com>
2019-12-15 18:39:10 +0900

    Refactor

--- main.cpp
+++ main.cpp
@@ -2,33 +2,38 @@
 #include <math.h>
 #include <alsa/asoundlib.h>
 
+#define SAMPLING_RATE 44100
+#define HELZ 440
+#define PERIODS 4
+#define PCM_NAME "default"
+#define PERIOD_SIZE 262144
+#define NUM_LOOP 100
+#define MODE_FLOAT true
+#define DEBUG false
+
 int main (int argc, char *argv[]) {
-  int rate = 44100;
-  int hz = 440;
-  unsigned exact_rate;
-  int periods = 4;
-  int num_frames;
-//  unsigned char *data;
-  float *data;
-  int l1, l2;
-  short s1, s2;
-  int frames;
-  char pcm_name[64] = "default";
+  int rate = SAMPLING_RATE;
+  int hz = HELZ;
+  int periods = PERIODS;
+  char pcm_name[64] = PCM_NAME;
+  snd_pcm_uframes_t periodsize = PERIOD_SIZE;
   snd_pcm_t *pcm_handle; 
   snd_pcm_stream_t stream = SND_PCM_STREAM_PLAYBACK;
   snd_pcm_hw_params_t *hwparams;
-//  snd_pcm_uframes_t periodsize = 8192 * 16;
-  snd_pcm_uframes_t periodsize = 8192 * 32;
   snd_pcm_uframes_t bufsize = periodsize * periods;
+  int i;
+#if MODE_FLOAT
+  float *data = (float *)malloc(periodsize * sizeof (float) * 2);
+#else
+#endif
 
-  std::cout << "Start my synth\n\n";
+  std::cout << "\nStart my synth\n\n";
 
+  printf ("==== setting ====\n");
   printf ("hz = %d\n", hz);
   printf ("periodsize = %lu\n", periodsize);
   printf ("bufsize = %lu\n", bufsize);
-
-//  pcm_name = argv[1];
-//  pcm_name = "default";
+  printf ("=================\n");
 
   snd_pcm_hw_params_alloca (&hwparams);
 
@@ -42,49 +47,37 @@ int main (int argc, char *argv[]) {
     return -1;
   }
 
-  if (snd_pcm_hw_params_set_access (pcm_handle, hwparams, SND_PCM_ACCESS_RW_INTERLEAVED) < 0) {
+  if (snd_pcm_hw_params_set_access (pcm_handle, hwparams,
+      SND_PCM_ACCESS_RW_INTERLEAVED) < 0) {
     std::cout << "error setting access\n";
     return -1;
   }
 
-//  if (snd_pcm_hw_params_set_format (pcm_handle, hwparams, SND_PCM_FORMAT_S16_LE) < 0) {
-  int r = snd_pcm_hw_params_set_format (pcm_handle, hwparams, SND_PCM_FORMAT_FLOAT);
-//  if (snd_pcm_hw_params_set_format (pcm_handle, hwparams, SND_PCM_FORMAT_FLOAT_LE) < 0) {
-  if (r < 0) {
+  if (snd_pcm_hw_params_set_format (pcm_handle, hwparams,
+      SND_PCM_FORMAT_FLOAT) < 0) {
     std::cout << "error setting format\n";
-    std::cout << "ret = " << r;
     return -1;
   }
 
-  exact_rate = rate;
-  if (snd_pcm_hw_params_set_rate_near(pcm_handle, hwparams, &exact_rate, 0) < 0) {
+  if (snd_pcm_hw_params_set_rate(pcm_handle, hwparams,
+      rate, 0) < 0) {
     std::cout << "error setting rate\n";
     return -1;
   }
 
-  if (rate != exact_rate) {
-    std::cout << "rate != exact_rate";
-  }
-
-  /* Set number of channels */
   if (snd_pcm_hw_params_set_channels(pcm_handle, hwparams, 2) < 0) {
     std::cout << "error setting channels\n";
     return -1;
   }
 
-  snd_pcm_uframes_t     buffer_size_max;
-  snd_pcm_hw_params_get_buffer_size_max(hwparams, &buffer_size_max);
-  printf ("bsm = %ld\n", buffer_size_max);
-
-  // doesn't work with snd_pcm_hw_params_set_buffer_size(). But don't know reason.
-  if (snd_pcm_hw_params_set_buffer_size_near(pcm_handle, hwparams, &bufsize) < 0) {
+  if (snd_pcm_hw_params_set_buffer_size_near(pcm_handle, hwparams,
+      &bufsize) < 0) {
     std::cout << "error setting buffersize\n";
     return(-1);
   }
 
-  printf ("bufsize = %lu\n", bufsize);
-
-  if (snd_pcm_hw_params_set_periods(pcm_handle, hwparams, periods, 0) < 0) {
+  if (snd_pcm_hw_params_set_periods(pcm_handle, hwparams, periods,
+      0) < 0) {
     std::cout << "error setting periods\n";
     return -1;
   }
@@ -94,40 +87,36 @@ int main (int argc, char *argv[]) {
     return(-1);
   }
 
-  data = (float *)malloc(periodsize * sizeof (float) * 2);
-//  frames = periodsize >> 2;
-  frames = periodsize;
-
-  printf ("frames=%d\n", frames);
-
-  int i;
-
-  for (i = 0; i <= frames * 2 + 3; i+=2) {
+#if MODE_FLOAT
+  for (i = 0; i <= periodsize * 2 + 3; i+=2) {
     data[i] = sin (2.0 * M_PI * i * hz / rate); 
     data[i+1] = data[i];
   }
+#else
+  for(int l2 = 0; l2 < periodsize; l2++) {
+    int s1, s2;
+    s1 = (l2 % 128) * 100 - 5000;
+    s2 = (l2 % 256) * 100 - 5000;
+    data[4*l2] = (unsigned char)s1;
+    data[4*l2+1] = s1 >> 8;
+    data[4*l2+2] = (unsigned char)s2;
+    data[4*l2+3] = s2 >> 8;
+  }
+#endif
 
-  for (i-=10; i <= frames * 2 + 3; i++ )
+#if DEBUG
+  for (i -= 10; i <= periodsize * 2 + 3; i++ )
     printf ("tail data[%d] = %f\n", i, data[i]);
-
-  for(l1 = 0; l1 < 100; l1++) {
-    printf ("l1 = %d\n", l1);
-/*
-    for(l2 = 0; l2 < frames; l2++) {
-      s1 = (l2 % 128) * 100 - 5000;
-      s2 = (l2 % 256) * 100 - 5000;
-      data[4*l2] = (unsigned char)s1;
-      data[4*l2+1] = s1 >> 8;
-      data[4*l2+2] = (unsigned char)s2;
-      data[4*l2+3] = s2 >> 8;
-    }
-*/
-    while ((snd_pcm_writei(pcm_handle, data, frames)) < 0) {
+#endif
+  for(int l1 = 0; l1 < NUM_LOOP; l1++) {
+    while ((snd_pcm_writei(pcm_handle, data, periodsize)) < 0) {
       snd_pcm_prepare(pcm_handle);
-      fprintf(stderr, "<<<<<<<<<<<<<<< Buffer Underrun >>>>>>>>>>>>>>>\n");
+      fprintf(stderr,
+          "<<<<<<<<<<<<<<< Buffer Underrun >>>>>>>>>>>>>>>\n");
     }
   }
 
+  free (data);
   snd_pcm_drop(pcm_handle);
   snd_pcm_drain(pcm_handle);
 }
